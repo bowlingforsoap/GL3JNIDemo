0f7a6352abafc3f39f03da0b3b76ff4a891b6da0
Strelchenko Vadym <strelchenkovadym@65.201.eduroam.dynamic.rbg.tum.de>
2016-05-03 16:16:00 +0200

    both: pm and stat are working

--- app/src/main/obj/local/armeabi-v7a/objs/gl3jni/main_simple.o.d
+++ app/src/main/obj/local/armeabi-v7a/objs/gl3jni/main_simple.o.d
@@ -1,2 +1,8 @@
 /Users/strelchenkovadym/AndroidStudioProjects/GL3JNIDemo/app/src/main/obj/local/armeabi-v7a/objs/gl3jni/main_simple.o: \
- /Users/strelchenkovadym/AndroidStudioProjects/GL3JNIDemo/app/src/main/jni/main_simple.cpp
+ /Users/strelchenkovadym/AndroidStudioProjects/GL3JNIDemo/app/src/main/jni/main_simple.cpp \
+ /Users/strelchenkovadym/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /Users/strelchenkovadym/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector
+
+/Users/strelchenkovadym/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/strelchenkovadym/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
